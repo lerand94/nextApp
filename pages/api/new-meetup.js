0319aaa7bea2ab34f1cb1a2c1b643d05d32ee9f0
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-09 22:31:21 +0300

    continue our API ( connected mongoDB )

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,23 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.h1ija4a.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "meetup inserted" });
   }
 }
 
