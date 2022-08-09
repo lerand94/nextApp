055f1713fe845e2b7a265faf1556f5dfaae8181f
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-09 18:44:58 +0300

    tested Link cmp from next

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News page</h1>;
+  return (
+    <Fragment>
+      <h1>The News page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js">NextJS is a great framework</Link>
+        </li>
+        <li>
+          <a href="/news/smth">smth else</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
