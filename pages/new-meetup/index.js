aaab1ce2050a922af7646288ec4b750a30397f7a
Kirill <27140985+lerand94@users.noreply.github.com>
2022-08-09 19:03:53 +0300

    created 3 pages and added some cmps for test rendering

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (data) => {
+    console.log(data);
+  };
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetupPage;
